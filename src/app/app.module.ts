96f79e442ef0eede10067099496341b7b7b85687
maxi <maximilianosalerno87@gmail.com>
2022-11-07 20:27:16 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ImagenComponent } from './component/imagen/imagen.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ImagenComponent,
+    
   ],
   imports: [
     BrowserModule
