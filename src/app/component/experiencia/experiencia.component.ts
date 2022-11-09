89b802fc963370dde169213fe5eb043376ab74c2
maxi <maximilianosalerno87@gmail.com>
2022-11-09 18:50:15 -0300

    creacion de componente experiencia

--- src/app/component/experiencia/experiencia.component.ts
+++ src/app/component/experiencia/experiencia.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienciaComponent implements OnInit {
 
+  trabajo = [{
+    empleo: 'Nombre del empleo',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, laboriosam.',
+    puesto: 'puesto',
+    periodo: '00/00/0000 - 00/00/0000'},
+    {
+    empleo: 'Nombre del empleo 2',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, laboriosam 2.',
+    puesto: 'puesto 2',
+    periodo: '00/00/0000 - 00/00/0000 2'},
+    {
+      empleo: 'Nombre del empleo 3',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, laboriosam 3.',
+    puesto: 'puesto 3',
+    periodo: '00/00/0000 - 00/00/0000 3'},]
   constructor() { }
 
   ngOnInit(): void {
