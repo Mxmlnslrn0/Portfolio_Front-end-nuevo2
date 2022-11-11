ce6a593ff080735663e80a93ce291067e54a6251
maxi <maximilianosalerno87@gmail.com>
2022-11-10 22:30:43 -0300

    imagenes y modificación de componente experiencia

--- src/app/component/experiencia/experiencia.component.ts
+++ src/app/component/experiencia/experiencia.component.ts
@@ -11,17 +11,20 @@ export class ExperienciaComponent implements OnInit {
     empleo: 'Nombre del empleo',
     descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, laboriosam.',
     puesto: 'puesto',
-    periodo: '00/00/0000 - 00/00/0000'},
+    periodo: '00/00/0000 - 00/00/0000',
+    imagen: 'assets/images/empresa1.png'},
     {
     empleo: 'Nombre del empleo 2',
     descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, laboriosam 2.',
     puesto: 'puesto 2',
-    periodo: '00/00/0000 - 00/00/0000 2'},
+    periodo: '00/00/0000 - 00/00/0000 2',
+    imagen: 'assets/images/empresa2.jpeg'},
     {
       empleo: 'Nombre del empleo 3',
     descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, laboriosam 3.',
     puesto: 'puesto 3',
-    periodo: '00/00/0000 - 00/00/0000 3'},]
+    periodo: '00/00/0000 - 00/00/0000 3',
+    imagen: 'assets/images/empresa3.png'},]
   constructor() { }
 
   ngOnInit(): void {
